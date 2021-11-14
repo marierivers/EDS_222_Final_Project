cb1ef6d3df844c06a80058be96f9d4b04e02b0d6
marierivers <mrivers@ucsb.edu>
2021-11-14 11:03:01 -0800

    joined eqi and population data

--- src/state_pop_fun.R
+++ src/state_pop_fun.R
@@ -9,8 +9,8 @@
 state_pop_fun <- function(fips) {
   path = paste0("co-est00int-01-", fips, ".csv")
   state_data <- read_csv(here("data", "population_data", "csv_files", path), skip = 5, col_names = header) %>% 
-    mutate(state_fips = fips) %>% 
+    mutate(state_code = fips) %>% 
     filter(april_1_2000 != "na") %>% 
-    mutate_at("geographic_area", str_replace, ".", "") %>% 
-    relocate(state_fips, .after = geographic_area)
+    mutate_at("county_name", str_replace, ".", "") %>% 
+    relocate(state_code, .after = county_name)
 }
