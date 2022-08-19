5eb75e73352631145ac1319026729eb5066e974d
marierivers <mrivers@bren.ucsb.edu>
2022-08-19 14:53:48 -0400

    cleaned up repo for csu

--- src/state_pop_fun.R
+++ src/state_pop_fun.R
@@ -1,14 +1,18 @@
-#' Title
+#' State Population Function 
 #'
-#' @param fips 
+#' @param fips # list of codes corresponding to each state
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#' 
 state_pop_fun <- function(fips) {
+  # create string for file path based on state fips code
   path = paste0("co-est00int-01-", fips, ".csv")
+  # read in data file for individual state
   state_data <- read_csv(here("data", "population_data", "csv_files", path), skip = 5, col_names = header) %>% 
+    # preliminary file cleaning
     mutate(state_code = fips) %>% 
     filter(april_1_2000 != "na") %>% 
     mutate_at("county_name", str_replace, ".", "") %>% 
